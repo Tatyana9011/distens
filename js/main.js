60fd7d7e80ea4b62d6cab81f1deee5babe776dd0
Tatyana9011 <75259849+Tatyana9011@users.noreply.github.com>
2020-12-19 11:00:38 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,12 @@
+const menybtn = document.querySelector(".min-meny");
+const meny = document.querySelector(".meny_saidebar");
+
+menybtn.addEventListener('click', function () {
+    meny.classList.toggle("active");
+});
+
+
+
+
+
+
